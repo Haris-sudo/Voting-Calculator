bb3d4883233ac62dfcc8c2fdd399383f82373b29
Ethan <19701638@students.lincoln.ac.uk>
2020-02-07 12:58:35 +0000

    CSV Reader and File Alterations
    Changed data filetype to CSV for easier processing. Created a reader for all data to be parsed through.

--- main.cs
+++ main.cs
@@ -1,27 +1,33 @@
 using System;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
 public class Country
 {
     // Conventions are as follows:
     // camelCase for regular variables, uppercase initial letter only on methods and functions
     // Add an _ for private variables
-
-    public string name;
-    public float popPercent;
-    private bool eurozoneStatus;
+    public string name { get; set; } 
+    public int population { get; set; }
+    public bool eurozoneStatus { get; set; }
     
     // Utilises a nullable bool for the vote, meaning we have three potential statuses of the bool.
     private bool? _vote;
     // This is perfect for the yes, no and abstain states of the vote.
+    
+    public Country(string name, int population, bool eurozoneStatus)
+    {
+
+    }
+    
 
     public void ChangeVote(bool newVote)
     {
         _vote = newVote;
     }
 
-    public bool GetVote()
+    public bool? GetVote()
     {
         return _vote;
     }
@@ -29,20 +35,34 @@ public class Country
 }
 
 
-public class Main
+public class Setup
 {
-    public void Setup()
+    static void Main()
     {
         List<Country> countryList = new List<Country>();
+        
+        using (var reader = new StreamReader("..\\Country Data.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            Country record = new Country("Placeholder", 0, false);
+
+            var records = csv.EnumerateRecords(record);
+
+            foreach (Country r in records)
+            {
+                Country newCountry = new Country(r.name, r.population, r.eurozoneStatus);
+                countryList.Add(newCountry);
+            }
+        }
+        /* 
 
         for(int i = 0; i<countryList.Count; i++)
         {
             countryList[i] = new Country();
         }
+        */
+    }
 
-     }
-
-    Console.Writeline("Sweaty balls");
 }
 
 
