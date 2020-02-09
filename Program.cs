43459fdaf4d2ab97edd2c36d920c07ff132233a2
Ethan <19701638@students.lincoln.ac.uk>
2020-02-09 14:24:37 +0000

    Separation of Country Class
    Separated the country class into its own file.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Voting_Calculator_Assignment
+{
+    class Program
+    {
+        static void Setup()
+        {
+            List<Country> countryList = new List<Country>();
+
+            using (var reader = new StreamReader("..\\Country Data.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                Country record = new Country("Placeholder", 0, false);
+
+                var records = csv.EnumerateRecords(record);
+
+                foreach (Country r in records)
+                {
+                    Country newCountry = new Country(r.name, r.population, r.eurozoneStatus);
+                    countryList.Add(newCountry);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Setup();
+        }
+    }
+}
