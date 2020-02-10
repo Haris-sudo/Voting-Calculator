fabc0fd7402b1dbfd238c4157e73c2c7aed0e01a
Gizzard <dominicohen@gmail.com>
2020-02-10 21:25:35 +0000

    Switched to terminal? [Y/N]
    Modified the JSON to allow us to make use of terminal commands, and created a simple Yes/No function to streamline development once the ball gets rolling.

--- Program.cs
+++ Program.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 
+
 namespace Voting_Calculator_Assignment
 {
     class Program
     {
         static void Setup()
         {
+            
             List<Country> countryList = new List<Country>();
 
-            using (var reader = new StreamReader("..\\Country Data.csv"))
+            using (var reader = new StreamReader("..\\Voting-Calculator-Assignment\\Data\\Country Data.csv"))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 Country record = new Country("Placeholder", 0, false);
@@ -27,9 +29,53 @@ namespace Voting_Calculator_Assignment
             }
         }
 
+        static bool YesNo(string text)
+        {
+            ConsoleKey choice;
+            do {
+            Console.Write(text);
+            choice = Console.ReadKey(false).Key;
+            if(choice != ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+            }
+
+            } while(choice != ConsoleKey.Y && choice != ConsoleKey.N);
+
+            if(choice == ConsoleKey.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         static void Main()
         {
-            Setup();
+            Console.WriteLine("This is fine.");
+            
+
+            //Setup();
+            
+
+            
+            bool votingComplete = false;
+
+            do { 
+                bool isEurozone = YesNo("Are all countries participating, or soley the eurozone members?(Y/N)");
+                
+
+                Console.WriteLine("{0} {1}", isEurozone.ToString(), funnyLolTest.ToString());
+
+                //votingComplete = true;
+
+
+
+            } while(!votingComplete);
+            Console.WriteLine("All done :)");
+
+            
         }
     }
 }
